c9d16c57ac43ebc8fb677f3850614e9776f7f9da
chenjm <cjiemin@163.com>
2020-04-30 10:19:57 +0800

    调整图片bug修复

--- CJMImageCategory/Classes/UIImage+CJMImageResize.h
+++ CJMImageCategory/Classes/UIImage+CJMImageResize.h
@@ -10,10 +10,17 @@
 @interface UIImage (CJMImageResize)
 
 /**
- * @brief 缩放图片
+ * @brief 调整图片
  * @param scale 缩放比例
- * @return 返回缩放图片，scale和imageOrientation跟原来的一样。
+ * @return 返回 image，image.scale = 1, image.imageOrientation = UIImageOrientationUp
  */
-- (UIImage *_Nonnull)cjmi_resizeImageWithScale:(CGFloat)scale;
+- (UIImage *_Nullable)cjmi_resizeImageWithScale:(CGFloat)scale;
+
+/**
+ * @brief 调整图片大小，如果图片像素的宽大于maxSize.width或者高大于maxSize.height。
+ * @param maxSize 图片的像素的最大值
+ * @return 返回 image，image.scale = 1, image.imageOrientation = UIImageOrientationUp
+ */
+- (UIImage *_Nullable)cjmi_resizeImageIfPixelSizeGreaterThan:(CGSize)maxSize;
 
 @end
